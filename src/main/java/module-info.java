7a4f49c98b9c25627f387da7cbf04d93f0e2232c
Mirko Caserta <mirko.caserta@gmail.com>
2021-06-02 13:46:13 +0200

    java module system enabled, closes #35

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.mirkocaserta.bruce {
+    exports com.mirkocaserta.bruce;
+    exports com.mirkocaserta.bruce.cipher;
+    exports com.mirkocaserta.bruce.cipher.asymmetric;
+    exports com.mirkocaserta.bruce.cipher.symmetric;
+    exports com.mirkocaserta.bruce.digest;
+    exports com.mirkocaserta.bruce.mac;
+    exports com.mirkocaserta.bruce.signature;
+}
