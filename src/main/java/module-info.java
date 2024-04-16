855fafe568bcaae79568dc509075d4c5a96c6754
Mirko Caserta <mirko.caserta@gmail.com>
2024-04-16 12:32:42 -0600

    fix: added missing package to exports

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module com.mirkocaserta.bruce {
     exports com.mirkocaserta.bruce;
+    exports com.mirkocaserta.bruce.annotations;
     exports com.mirkocaserta.bruce.cipher;
     exports com.mirkocaserta.bruce.cipher.asymmetric;
     exports com.mirkocaserta.bruce.cipher.symmetric;
